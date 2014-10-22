058c23b150e6e972a504c2a43c32b609a9fdf1e9
rrmccrary@gmail.com <rrmccrary@gmail.com>
2014-10-22 12:17:36 -0400

    again

--- database.rb
+++ database.rb
@@ -5,7 +5,7 @@ class Database
     DataMapper::Logger.new($stdout, :debug)
     # memory database appears to timeout if not used in ~5 mins, which causes the database to be dumped
     #DataMapper.setup(:default, 'sqlite::memory:')
-    DataMapper.setup(:default, 'sqlite:test.db')
+    DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
     DataMapper.finalize
     DataMapper.auto_upgrade!
   end
